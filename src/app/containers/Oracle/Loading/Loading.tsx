c0d99b0f91b4bd7a5ed9a3ffcee0315c547c9a31
vba2000 <vba@mail.ru>
2018-12-06 22:55:28 +0300

    Fix redirects

--- src/app/containers/Oracle/Loading/Loading.tsx
+++ src/app/containers/Oracle/Loading/Loading.tsx
@@ -1,21 +1,8 @@
 import * as React from 'react';
 import { Spin } from 'antd';
-import { ORACLE_STATUS } from 'app/models';
-import { Redirect } from 'react-router';
 
 
-export const Loading: React.StatelessComponent<IProps> = ({ tip, size, status }) => {
-    
-    switch (status) {
-        case ORACLE_STATUS.EMPTY:
-            return <Redirect to={'/oracle/empty'}/>;
-        case ORACLE_STATUS.HAS_ERROR:
-            return <Redirect to={'/oracle/create'}/>;
-        case ORACLE_STATUS.READY:
-            return <Redirect to={'/oracle/edit'}/>;
-        case ORACLE_STATUS.SERVER_ERROR:
-            return <Redirect to={'/oracle/error'}/>;
-    }
+export const Loading: React.StatelessComponent<IProps> = ({ tip, size }) => {
     
     return <div>
         <Spin size={size} tip={tip}/>
@@ -25,5 +12,4 @@ export const Loading: React.StatelessComponent<IProps> = ({ tip, size, status })
 interface IProps {
     tip?: string;
     size?: "default" | "small" | "large";
-    status: ORACLE_STATUS;
 }
--- src/app/containers/Oracle/index.tsx
+++ src/app/containers/Oracle/index.tsx
@@ -54,12 +54,25 @@ export class OracleApp extends React.Component<OracleApp.Props> {
         const { oracleInfo } = this.props;
         const path = this.props.history.location.pathname;
         const menu = <OracleMenu path={path} history={this.props.history} address={address} name={name}/>;
+    
+        if (path === '/oracle') {
+            switch (oracleInfo.status) {
+                case ORACLE_STATUS.EMPTY:
+                    return <Redirect to={'/oracle/empty'}/>;
+                case ORACLE_STATUS.HAS_ERROR:
+                    return <Redirect to={'/oracle/create'}/>;
+                case ORACLE_STATUS.READY:
+                    return <Redirect to={'/oracle/tokens'}/>;
+                case ORACLE_STATUS.SERVER_ERROR:
+                    return <Redirect to={'/oracle/error'}/>;
+            }
+        }
         
         return (
             <LayoutComponent leftSider={menu}>
                 <Switch>
                     <Route path="/oracle" exact>
-                        <Loading status={oracleInfo.status}/>
+                        <Loading/>
                     </Route>
                     
                     <ConditionRouter condition={oracleInfo.status === ORACLE_STATUS.EMPTY}
